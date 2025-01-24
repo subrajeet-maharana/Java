caee47b04594e1e2a43cb5039cf7e5474c0f7fb4
subrajeet-maharana <subrajeet.info@gmail.com>
2025-01-24 16:10:27 +0530

    added default and parameterized constructor

--- 01classesAndObjects/User.java
+++ 01classesAndObjects/User.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
 
 class User {
-  String name = "Default User Name";
-  String email = "Default User Email";
-  int age = 0;
+  String name;
+  String email;
+  int age;
+
+  User() {
+    name = "Default Name";
+    email = "Default Email";
+    age = 25;
+  }
+
+  User(String customName, String customEmail, int customAge) {
+    name = customName;
+    email = customEmail;
+    age = customAge;
+  }
 
   public void updateEmail(String updatedEmail) {
     email = updatedEmail;
@@ -12,10 +24,11 @@ class User {
   public static void main(String[] args) {
     User userOne = new User();
     System.out.println("User Name: " + userOne.name + "\nUser Email: " + userOne.email + "\nUser Age: " + userOne.age);
-  Scanner sc = new Scanner(System.in);
-  System.out.println("Enter your updated email: ");
-  String updatedEmail = sc.nextLine();
-  userOne.updateEmail(updatedEmail);
-  System.out.println("User Name: " + userOne.name + "\nUser Email: " + userOne.email + "\nUser Age: " + userOne.age);
+    User userTwo = new User("Subrajeet Maharana", "subrajeet.maharana@gmail.com", 24);
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your updated email: ");
+    String updatedEmail = sc.nextLine();
+    userTwo.updateEmail(updatedEmail);
+    System.out.println("User Name: " + userTwo.name + "\nUser Email: " + userTwo.email + "\nUser Age: " + userTwo.age);
   }
 }
