c054bd48431c757573139b1f592961d4d4737e3e
subrajeet-maharana <subrajeet.info@gmail.com>
2025-01-24 20:30:55 +0530

    added totalUsers counter (static)

--- 01classesAndObjects/User.java
+++ 01classesAndObjects/User.java
@@ -4,17 +4,20 @@ class User {
   String name;
   String email;
   int age;
+  public static int totalUsers = 0;
 
   User() {
     name = "Default Name";
     email = "Default Email";
     age = 25;
+    totalUsers++;
   }
 
   User(String customName, String customEmail, int customAge) {
     name = customName;
     email = customEmail;
     age = customAge;
+    totalUsers++;
   }
 
   public void updateEmail(String updatedEmail) {
@@ -24,11 +27,13 @@ class User {
   public static void main(String[] args) {
     User userOne = new User();
     System.out.println("User Name: " + userOne.name + "\nUser Email: " + userOne.email + "\nUser Age: " + userOne.age);
+    System.out.println("Total Users: " + totalUsers);
     User userTwo = new User("Subrajeet Maharana", "subrajeet.maharana@gmail.com", 24);
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter your updated email: ");
     String updatedEmail = sc.nextLine();
     userTwo.updateEmail(updatedEmail);
     System.out.println("User Name: " + userTwo.name + "\nUser Email: " + userTwo.email + "\nUser Age: " + userTwo.age);
+    System.out.println("Total Users: " + totalUsers);
   }
 }
