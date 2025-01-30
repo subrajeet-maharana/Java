497bff9a5f75c5096c912f76dd8b6e5cadc8afd9
subrajeet-maharana <subrajeet.info@gmail.com>
2025-01-30 21:25:14 +0530

    added BankAccount class

---
+++ 01classesAndObjects/BankAccount.java
@@ -0,0 +1,29 @@
+public class BankAccount {
+  private int balance;
+
+  BankAccount(int initialDeposit) {
+    balance = initialDeposit;
+    System.out.println("Bank Account created successfully with balance: " + balance);
+  }
+
+  public void deposit(int amount) {
+    balance = balance + amount;
+    System.out.println("Updated Balance after deposit: " + balance);
+  }
+
+  public void withdraw(int amount) {
+    if (amount>balance) {
+      System.err.println("Insufficient Balance");
+      return;
+    }
+    balance = balance - amount;
+    System.out.println("Updated Balance after withdrawal: " + balance);
+  }
+
+  public static void main(String[] args) {
+    BankAccount newAcc = new BankAccount(56400);
+    newAcc.deposit(8994);
+    newAcc.withdraw(500000);
+    newAcc.withdraw(8457);
+  }
+}
