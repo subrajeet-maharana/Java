169ef6b323af7f8ee16bb8e7da1ea9743d18eb53
subrajeet-maharana <subrajeet.info@gmail.com>
2025-02-04 17:20:52 +0530

    added SmartHome class

---
+++ 02InheritanceAndPolymorphism/SmartHome.java
@@ -0,0 +1,29 @@
+class SmartDevice {
+  String deviceName = "My Smart Device";
+  boolean isOn = false;
+
+  SmartDevice(String name, boolean status) {
+    deviceName = name;
+    isOn = status;
+  }
+
+  protected void turnOn() {
+    isOn = true;
+  }
+  
+  protected void turnOff() {
+    isOn = false;
+  }
+
+  protected String getStatus() {
+    return "SmartDevice [" + deviceName + "] is [" + (isOn?"ON":"OFF") + "]";
+  }
+}
+
+public class SmartHome {
+  public static void main(String[] args) {
+    SmartDevice dev1 = new SmartDevice("My Sweet Device", false);
+    String status = dev1.getStatus();
+    System.out.println(status);
+  }
+}
