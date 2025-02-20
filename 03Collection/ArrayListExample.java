7412f13559b58170b9f15ec4c7410dd92a9c2f90
subrajeet-maharana <subrajeet.info@gmail.com>
2025-02-20 17:35:15 +0530

    sorting in names ArrayList

--- 03Collection/ArrayListExample.java
+++ 03Collection/ArrayListExample.java
@@ -2,28 +2,32 @@ import java.util.*;
 
 public class ArrayListExample {
   public static void main(String[] args) {
-    ArrayList<String> ls = new ArrayList<String>();
-    ls.add("Subrajeet");
-    ls.add("Amit");
-    ls.add("Madook");
-    ls.remove("Amit");
-    ls.add("Chavva");
-    System.out.println(ls);
+    ArrayList<String> names = new ArrayList<String>();
+    names.add("Subrajeet");
+    names.add("Amit");
+    names.add("Madook");
+    names.remove("Amit");
+    names.add("Chavva");
+    System.out.println(names);
 
     // Iteration using Iterator Class
-    Iterator<String> itr = ls.iterator();
+    Iterator<String> itr = names.iterator();
     while(itr.hasNext()) {
       System.out.println(itr.next());
     }
 
     // Iteration using for-each loop
-    for(String name:ls) {
+    for(String name:names) {
       System.out.println(name);
     }
 
     //Getter and Setter
-    System.out.println("The value at index 1 is: " + ls.get(1));
-    ls.set(1, "Warner Bros");
-    System.out.println("The value at index 1 after setting is: " + ls.get(1));
+    System.out.println("The value at index 1 is: " + names.get(1));
+    names.set(1, "Warner Bros");
+    System.out.println("The value at index 1 after setting is: " + names.get(1));
+
+    //Sorting
+    Collections.sort(names);
+    System.out.println("After Sorting: " + names);
   }
 }
