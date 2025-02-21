9f5d21734c0320127ee9cce506af46ce75ffea0e
subrajeet-maharana <subrajeet.info@gmail.com>
2025-02-21 21:30:35 +0530

    explored all looping constructs in ArrayList

--- 03Collection/ArrayListExample.java
+++ 03Collection/ArrayListExample.java
@@ -10,16 +10,33 @@ public class ArrayListExample {
     names.add("Chavva");
     System.out.println(names);
 
-    // Iteration using Iterator Class
+    System.out.println("Iteration using Iterator Class"); 
     Iterator<String> itr = names.iterator();
     while(itr.hasNext()) {
       System.out.println(itr.next());
     }
 
-    // Iteration using for-each loop
+    System.out.println("Iteration using for-each loop"); 
     for(String name:names) {
       System.out.println(name);
     }
+
+    System.out.println("Iteration using ListIterator");
+    ListIterator<String> lsItr = names.listIterator(names.size());
+    while(lsItr.hasPrevious()) {
+      String name = lsItr.previous();
+      System.out.println(name); 
+    }
+
+    System.out.println("Iteration using for loop");
+    for(int i=0;i<names.size();++i) {
+      System.out.println(names.get(i));
+    } 
+
+    System.out.println("Iteration using foreach loop");
+    names.forEach(a->{
+      System.out.println(a);
+    });
 
     //Getter and Setter
     System.out.println("The value at index 1 is: " + names.get(1));
